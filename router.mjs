d534799eb27b2b8d47dea3cce78ea8caf0e3e5f9
d1mazzz <dmitriy.b@blaize.tech>
2022-11-29 18:21:59 +0200

    feature(router): collect webhook endpoint post data

--- router.mjs
+++ router.mjs
@@ -7,12 +7,21 @@ export const router = {
     // called from clickUp
     // call github api dependent on coming event
     "/webhook": (req, res) => {
-      console.log("WEBHOOK-GET: ", { req });
+      console.log("WEBHOOK-GET");
     },
   },
   POST: {
     "/webhook": (req, res) => {
-      console.log("WEBHOOK-POST: ", { req });
+      console.log("WEBHOOK-POST");
+      let data = "";
+
+      req.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      req.on("end", () => {
+        console.log("WEBHOOK payload", { data, string: data.toString(), obj: JSON.parse(data.toString()) });
+      });
     },
   },
 };
